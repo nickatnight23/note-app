356634ab171a2a1f1ee752953eb2d059f7100db1
Latricia <tricianickelberry@gmail.com>
2021-08-22 10:55:23 -0700

    add note

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk')
 const { demandOption } = require('yargs')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // customize yargs version
 yargs.version('1.1.0')
@@ -24,8 +24,7 @@ yargs.command({
 
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
